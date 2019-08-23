ad2433f9633e358bec3616a3e43ef59448713258
BinaryShrimp <arandombinaryshrimp@gmail.com>
2019-08-22 23:19:55 -0500

    added landing page

---
+++ menu-toggle.js
@@ -0,0 +1,5 @@
+// When "search player" is pressed
+document.querySelector('.search-player').addEventListener('click', function() {
+    document.querySelector('.landing-page').classList.add('hide');
+    document.querySelector('.Search-player-section').classList.remove('hide'); 
+});
